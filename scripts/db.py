a89e1492e9e271c5d3f38e480acccda0ab2b52c1
Christian <Christianmontanaro390@gmail.com>
2025-02-05 23:01:03 +0100

    added connection

--- scripts/db.py
+++ scripts/db.py
@@ -1,5 +1,26 @@
+from flask import Flask
+from flask_mysqldb import MySQL
 
-"""
+app = Flask()
+
+
+app.config['MYSQL_HOST'] = "localhost"
+app.config['MYSQL_USER'] = "user"
+app.config['MYSQL_PASSWORD'] = "pswd"
+app.config['MYSQL_DB'] = "w3schools"
+app.config['MYSQL_PORT'] = 3306
+mysql = MySQL()
+conn = mysql.connection
+cursor = conn.cursor()
+
+
+
+Query = """
+
+CREATE DATABASE Biblioteca;
+
+USE Biblioteca;
+
 CREATE TABLE IF NOT EXISTS Tessere(
         idTessera CHAR(36),
         email varchar(255) NOT NULL,
@@ -54,6 +75,8 @@ CREATE TABLE IF NOT EXISTS Prestiti(
         FOREIGN KEY (idTessera) REFERENCES Tessere(idTessera) ON DELETE CASCADE
 );
 
-
-
 """
+
+cursor.execute(Query)
+conn.commit()
+cursor.close()
